d569719a7b009e934c69d1928536e9633a813c40
Maxence Leroy <maxence.leroy3@gmail.com>
2023-11-03 09:20:10 +0100

    split logging functions

--- potato/src/logging.py
+++ potato/src/logging.py
@@ -6,24 +6,27 @@ info = True
 warning = True
 error = True
 
-def print_with_date(log: str):
-  now = datetime.now()
-  print(f'[{now.hour}:{now.minute}:{now.second}:{now.microsecond}] {log}')
+def log_with_date(log: str):
+    now = datetime.now()
+    print_log(f'[{now.hour}:{now.minute}:{now.second}:{now.microsecond}] {log}')
 
-destination_function: Callable[[str], None] = print_with_date
+def print_log(log: str):
+    print(log)
+
+destination_function: Callable[[str], None] = log_with_date
 
 def logging_debug(log: str):
-  if debug:
-    destination_function(f'[D] : {log}')
+    if debug:
+        destination_function(f'[D] : {log}')
 
 def logging_info(log: str):
-  if info:
-    destination_function(f'[I] : {log}')
+    if info:
+        destination_function(f'[I] : {log}')
 
 def logging_warning(log: str):
-  if warning:
-    destination_function(f'[W] : {log}')
+    if warning:
+        destination_function(f'[W] : {log}')
 
 def logging_error(log: str):
-  if error:
-    destination_function(f'[E] : {log}')
+    if error:
+        destination_function(f'[E] : {log}')
