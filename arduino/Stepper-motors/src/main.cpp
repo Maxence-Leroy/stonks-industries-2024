dfb42acc645d2379320b3a0632e6f84ab3da89b4
Maxence Leroy <maxence.leroy3@gmail.com>
2023-11-09 10:17:32 +0100

    basic enslavement of the robot

--- arduino/Stepper-motors/src/main.cpp
+++ arduino/Stepper-motors/src/main.cpp
@@ -1,20 +1,27 @@
 #include <Arduino.h>
 
+#include "helpers/path/line.h"
+#include "helpers/path/rotation.h"
 #include "readingComponents/accelero.h"
 #include "readingComponents/incrementalEncoder.h"
+#include "movingComponents/enslavement.h"
 #include "movingComponents/stepperMotors.h"
 
-void setup()
+Path* path;
 
+void setup()
 {
   setupMotors();
   setupAccelero();
   setupIncrementalEncoders();
-  setLeftMotorSpeed(5000);
-  setRightMotorSpeed(10000);
+  setInitialPosition(0, 0, Angle(0));
+  // path = new Rotation(0, 0, Angle(0), Angle(M_PI / 2), 100, 100);
+  path = new Line(0, 0, 100, 0, 10, 1);
+  setCurrentPath(path);
+  path->start();
 }
 
 void loop()
 {
-
+  enslave(micros());
 }
