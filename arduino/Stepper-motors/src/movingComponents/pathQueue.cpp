1611242be92a45878b784302d27274d0c44db143
Maxence Leroy <maxence.leroy3@gmail.com>
2023-11-11 18:38:27 +0100

    add rotation to go to the right direction

--- arduino/Stepper-motors/src/movingComponents/pathQueue.cpp
+++ arduino/Stepper-motors/src/movingComponents/pathQueue.cpp
@@ -12,6 +12,8 @@ Queue<Path>* queue = new Queue<Path>();
 
 const double maxSpeed = 500;
 const double maxAcceleration = 500;
+const double maxSpeedRotation = 2;
+const double maxAccelerationRotation = 2;
 
 void addDestination(double x, double y, Angle theta, bool backwards)
 {
@@ -19,18 +21,42 @@ void addDestination(double x, double y, Angle theta, bool backwards)
     double currentY = getCurrentY();
     Angle currentTheta = getCurrentTheta();
 
+    Angle requiredTheta = currentTheta;
+
     if(fabs(x - currentX) > 0.5 || fabs(y - currentY) > 0.5) 
     {
+        requiredTheta = Angle::computeAngle(currentX, currentY, x, y);
+        if(backwards)
+        {
+            currentTheta += M_PI;
+        }
+        if(fabs((requiredTheta - currentTheta).toDouble()))
+        {
+            if(LOGGING)
+            {
+                Serial.print("New rotation to ");
+                Serial.println(requiredTheta.toDouble());
+            }
+            queue->add(new Rotation(currentX, currentY, currentTheta, requiredTheta, maxSpeedRotation, maxAccelerationRotation));
+        }
         if(LOGGING)
         {
             Serial.println("New line");
         }
         queue->add(new Line(currentX, currentY, x, y, backwards ? -maxSpeed : maxSpeed, maxAcceleration));
+        if(backwards)
+        {
+            currentTheta -= M_PI;
+        }
     }
 
     if(fabs((currentTheta - theta).toDouble() > 0.05)) 
     {
-        queue->add(new Rotation(x, y, currentTheta, theta, maxSpeed, maxAcceleration));
+        if(LOGGING)
+        {
+            Serial.println("New rotation");
+        }
+        queue->add(new Rotation(x, y, requiredTheta, theta, maxSpeedRotation, maxAccelerationRotation));
     }
 }
 
