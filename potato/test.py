4ed9dbb20f9a96467b09764755c0a2378e03d8a9
Maxence Leroy <maxence.leroy3@gmail.com>
2023-11-14 18:04:40 +0100

    workingg field d* on 60*40

--- potato/test.py
+++ potato/test.py
@@ -1,5 +1,6 @@
 # pyright: reportUnknownMemberType=false
 
+from functools import reduce
 import matplotlib
 matplotlib.use('MacOSX')
 import math
@@ -10,8 +11,8 @@ from queue import PriorityQueue
 import time
 from typing import Self
 
-WIDTH = 30
-DEPTH = 20
+WIDTH = 60
+DEPTH = 40
 class State:
     value: tuple[int, int]
     succs: list[Self]
@@ -47,7 +48,7 @@ class InterpolatedState(State):
 
     def to_float(self) -> tuple[float, float]:
         return self.float_value
-
+    
 class DStarLight:
     rhs: NDArray[Shape["300,200"], Float]
     g: NDArray[Shape["300,200"], Float]
@@ -59,26 +60,49 @@ class DStarLight:
     costs: NDArray[Shape["300,200"], Float]
     path: list[State]
     points_in_u: dict[State, tuple[float, float]]
+    connbr: dict[State, list[tuple[State, State]]]
+    path: list[State]
 
-    # def plot(self):
-    #     pl.subplots(1, 1, figsize=(10,10))
-    #     pl.plot(self.s_goal.value[0], self.s_goal.value[1], "ob")
-    #     pl.plot(self.s_start.value[0], self.s_start.value[1], "og")
-    #     for x in range(0,30):
-    #         for y in range(0,20):
-    #             pl.text(x, y, f"{round(self.g[x, y], 2)}\n{round(self.rhs[x, y], 2)}", color="red", fontsize=6)
-        
-    #     pl.pause(0.00000001)
-    #     print("Wait")
+    def plot(self):
+        pl.grid(True)
+        pl.axis("Equal")
+        pl.xlim([0,WIDTH])
+        pl.ylim([0,DEPTH])
+        pl.plot(self.s_goal.value[0], self.s_goal.value[1], "ob")
+        pl.plot(self.s_start.value[0], self.s_start.value[1], "og")
+        for x in range(0,WIDTH):
+            for y in range(0,DEPTH):
+                color = "green" if State(x,y) in self.recomputed else "red"
+                pl.text(x, y, f"{round(self.g[x, y], 2)}\n{round(self.rhs[x, y], 2)}", color=color, fontsize=5)
+        pl.imshow(self.costs.transpose() > 1, aspect='auto', interpolation='nearest' )
+        pl.pause(0.0001)
+        print("Wait")
+        pl.clf()
 
     def heuristic(self, p: State, q: State) -> float:
-        return round(math.sqrt((p.to_float()[0] - q.to_float()[0]) ** 2 + (p.to_float()[1] - q.to_float()[1]) ** 2), 3)
+        start = time.time()
+        res = round(math.sqrt((p.to_float()[0] - q.to_float()[0]) ** 2 + (p.to_float()[1] - q.to_float()[1]) ** 2), 3)
+        self.time_in_heuristic += time.time() - start
+        return res
 
     def calculate_key(self, s: State) -> tuple[float, float]:
+        start = time.time()
         (a, b) = (min(self.g[s.value],self.rhs[s.value]) + self.heuristic(self.s_start, s) + self.k_m , min(self.g[s.value], self.rhs[s.value]))
-        return (round(a, 20), round(b, 20))
+        self.time_in_calculate_key += time.time() - start
+        return (a, b)
     
     def __init__(self, s_start: State, s_goal: State, costs: NDArray[Shape["300,200"], Float]) -> None:
+        self.time_in_heuristic = 0
+        self.time_in_calculate_key = 0
+        self.time_in_valid_state = 0
+        self.time_in_prev = 0
+        self.time_in_c = 0
+        self.time_in_connbrs = 0
+        self.time_in_update_vertex = 0
+        self.time_in_compute_shortest_path = 0
+        self.recomputed = set()
+        self.connbr = {}
+        start = time.time()
         self.u = PriorityQueue()
         self.points_in_u = {}
         self.path = []
@@ -92,12 +116,17 @@ class DStarLight:
         goal_key = self.calculate_key(self.s_goal)
         self.points_in_u[self.s_goal] = goal_key
         self.u.put_nowait((goal_key, self.s_goal))
+        self.time_in_init = time.time() - start
 
     def state_is_valid(self, u: State) -> bool:
+        start = time.time()
         (x, y) = u.to_float()
-        return 0 <= x <= WIDTH - 1 and 0 <= y <= DEPTH - 1
+        res = 0 <= x <= WIDTH - 1 and 0 <= y <= DEPTH - 1
+        self.time_in_valid_state += time.time() - start
+        return res
 
     def prev(self, u: State) -> list[State]:
+        start = time.time()
         (x, y) = u.value
         prevs = [
             State(x-1, y-1),
@@ -110,7 +139,9 @@ class DStarLight:
             State(x-1, y),
         ]
 
-        return list(filter(lambda u: self.state_is_valid(u), prevs))
+        res = list(filter(lambda u: self.state_is_valid(u), prevs))
+        self.time_in_prev += time.time() - start
+        return res
     
     def is_diagonal(self, s1: State, s2: State) -> bool:
         diff_x = abs(s2.value[0] - s1.value[0])
@@ -118,6 +149,7 @@ class DStarLight:
         return diff_x + diff_y == 2
 
     def c(self, s: State, s_a: State, s_b: State) -> float:
+        start = time.time()
         if self.is_diagonal(s_a, s):
             s_1 = s_b
             s_2 = s_a
@@ -143,12 +175,15 @@ class DStarLight:
                     v_s = c * math.sqrt(2) + self.g[s_2.value]
                 else:
                     x = 1 - min(b/ math.sqrt(c*c-b*b), 1)
-                    v_s = c * math.sqrt(1 + (1-x)**2) + b * x + self.g[s_2.value]
+                    v_s = c * math.sqrt(1 + (1-x)*(1-x)) + b * x + self.g[s_2.value]
 
+        self.time_in_c += time.time() - start
         return v_s
     
-    def connbrs(self, s:State) -> list[tuple[State, State]]:
+    def connbrs(self, s:State, after_obstacle: bool) -> list[tuple[State, State]]:
+        start = time.time()
         nbrs: list[tuple[State, State]] = []
+
         s_1 = State(s.value[0] + 1, s.value[1])
         s_2 =  State(s.value[0] + 1, s.value[1] + 1)
         s_3 =  State(s.value[0], s.value[1] + 1)
@@ -158,44 +193,72 @@ class DStarLight:
         s_7 =  State(s.value[0], s.value[1] - 1)
         s_8 =  State(s.value[0] + 1, s.value[1] - 1)
 
-        if self.state_is_valid(s_1) and self.state_is_valid(s_2) and (s_1 in s.succs or s_2 in s.succs):
-            nbrs.append((s_1, s_2))
+        if not after_obstacle:
+            if s in self.connbr:
+                self.time_in_connbrs += time.time() - start
+                return self.connbr[s]
 
-        if self.state_is_valid(s_2) and self.state_is_valid(s_3) and (s_2 in s.succs or s_3 in s.succs):
-            nbrs.append((s_2, s_3))
+            if self.state_is_valid(s_1) and self.state_is_valid(s_2) and (s_1 in s.succs or s_2 in s.succs):
+                nbrs.append((s_1, s_2))
 
-        if self.state_is_valid(s_3) and self.state_is_valid(s_4) and (s_3 in s.succs or s_4 in s.succs):
-            nbrs.append((s_3, s_4))
+            if self.state_is_valid(s_2) and self.state_is_valid(s_3) and (s_2 in s.succs or s_3 in s.succs):
+                nbrs.append((s_2, s_3))
 
-        if self.state_is_valid(s_4) and self.state_is_valid(s_5) and (s_4 in s.succs or s_5 in s.succs):
-            nbrs.append((s_4, s_5))
+            if self.state_is_valid(s_3) and self.state_is_valid(s_4) and (s_3 in s.succs or s_4 in s.succs):
+                nbrs.append((s_3, s_4))
 
-        if self.state_is_valid(s_5) and self.state_is_valid(s_6) and (s_5 in s.succs or s_6 in s.succs):
-            nbrs.append((s_5, s_6))
+            if self.state_is_valid(s_4) and self.state_is_valid(s_5) and (s_4 in s.succs or s_5 in s.succs):
+                nbrs.append((s_4, s_5))
 
-        if self.state_is_valid(s_6) and self.state_is_valid(s_7) and (s_6 in s.succs or s_7 in s.succs):
-            nbrs.append((s_6, s_7))
+            if self.state_is_valid(s_5) and self.state_is_valid(s_6) and (s_5 in s.succs or s_6 in s.succs):
+                nbrs.append((s_5, s_6))
 
-        if self.state_is_valid(s_7) and self.state_is_valid(s_8) and (s_7 in s.succs or s_8 in s.succs):
-            nbrs.append((s_7, s_8))
+            if self.state_is_valid(s_6) and self.state_is_valid(s_7) and (s_6 in s.succs or s_7 in s.succs):
+                nbrs.append((s_6, s_7))
 
-        if self.state_is_valid(s_8) and self.state_is_valid(s_1) and (s_8 in s.succs or s_1 in s.succs):
-            nbrs.append((s_8, s_1))
+            if self.state_is_valid(s_7) and self.state_is_valid(s_8) and (s_7 in s.succs or s_8 in s.succs):
+                nbrs.append((s_7, s_8))
 
-        return nbrs
+            if self.state_is_valid(s_8) and self.state_is_valid(s_1) and (s_8 in s.succs or s_1 in s.succs):
+                nbrs.append((s_8, s_1))
 
-    def update_vertex(self, u: State) -> None:
-        # print(f"update {u} {self.rhs[u.value]}")
+            self.connbr[s] = nbrs
+            self.time_in_connbrs += time.time() - start
+            return nbrs
+        else:
+            if self.state_is_valid(s_1) and self.state_is_valid(s_2):
+                nbrs.append((s_1, s_2))
+
+            if self.state_is_valid(s_2) and self.state_is_valid(s_3):
+                nbrs.append((s_2, s_3))
+
+            if self.state_is_valid(s_3) and self.state_is_valid(s_4):
+                nbrs.append((s_3, s_4))
+
+            if self.state_is_valid(s_4) and self.state_is_valid(s_5):
+                nbrs.append((s_4, s_5))
+
+            if self.state_is_valid(s_5) and self.state_is_valid(s_6):
+                nbrs.append((s_5, s_6))
+
+            if self.state_is_valid(s_6) and self.state_is_valid(s_7):
+                nbrs.append((s_6, s_7))
+
+            if self.state_is_valid(s_7) and self.state_is_valid(s_8):
+                nbrs.append((s_7, s_8))
+
+            if self.state_is_valid(s_8) and self.state_is_valid(s_1):
+                nbrs.append((s_8, s_1))
+
+            self.connbr[s] = nbrs
+            self.time_in_connbrs += time.time() - start
+            return nbrs
+
+    def update_vertex(self, u: State, after_obstacle: bool) -> None:
+        start = time.time()
         if u.value != self.s_goal.value:
-            min_value = np.inf
-            for (s_prime, s_prime_prime) in self.connbrs(u):
-                value = self.c(u, s_prime, s_prime_prime)
-                if value <= min_value:
-                    min_value = value
-            if self.rhs[u.value] > min_value:
-                # print("Change rhs")
-                self.rhs[u.value] = min_value
-                #elf.plot()
+            min_value = min([self.c(u, s_prime, s_prime_prime) for (s_prime, s_prime_prime) in self.connbrs(u, after_obstacle)])
+            self.rhs[u.value] = min_value
 
         try:
             key = self.points_in_u[u]
@@ -205,13 +268,17 @@ class DStarLight:
                 
         if self.g[u.value] != self.rhs[u.value]:
             key = self.calculate_key(u)
-            if key[0] != np.inf:
-                u_key = key
-                if (u_key, u) not in self.u.queue:
-                    self.points_in_u[u] = key
+            u_key = key
+            if (u_key, u) not in self.u.queue:
+                self.points_in_u[u] = key
+                if u not in self.recomputed:
                     self.u.put_nowait((key, u))
+                    if after_obstacle:
+                        self.recomputed.add(u)
+        self.time_in_update_vertex += time.time() - start
 
-    def compute_shortest_path(self):
+    def compute_shortest_path(self, after_obstacle: bool):
+        start = time.time()
         while self.u.queue[0][0] < self.calculate_key(self.s_start) or self.rhs[self.s_start.value] != self.g[self.s_start.value]:
             _, u = self.u.get_nowait()
             self.points_in_u.pop(u)
@@ -221,14 +288,17 @@ class DStarLight:
                 previouses = self.prev(u)
                 for prev in previouses:
                     prev.succs.append(u)
-                    self.update_vertex(prev)
-            else:
-                self.g[u.value] = np.inf
-                self.update_vertex(u)
-                previouses = self.prev(u)
-                for prev in previouses:
-                    prev.succs.append(u)
-                    self.update_vertex(prev)
+                    self.update_vertex(prev, after_obstacle)
+            # else:
+            #     self.g[u.value] = np.inf
+            #     self.update_vertex(u, after_obstacle)
+            #     previouses = self.prev(u)
+            #     for prev in previouses:
+            #         prev.succs.append(u)
+            #         self.update_vertex(prev, after_obstacle)
+        self.u.queue = []
+        self.points_in_u = {}
+        self.time_in_compute_shortest_path += time.time() - start
 
     def get_best_interpolated_child(self, s: State) -> InterpolatedState:
         interpolated_children: PriorityQueue[tuple[float, InterpolatedState]] = PriorityQueue()
@@ -262,22 +332,22 @@ class DStarLight:
             child = InterpolatedState(x + i /division, y + 1)
             if self.state_is_valid(child):
                 if i < 0:
-                    cost = self.heuristic(child, s) + abs(i/division) * self.g[x-1,y+1] + (1-abs(i/division)) * self.g[x, y+1]
+                    cost = self.heuristic(child, s) + abs(i/division) * self.rhs[x-1,y+1] + (1-abs(i/division)) * self.rhs[x, y+1]
                 elif i > 0:
-                    cost = self.heuristic(child, s) + abs(i/division) * self.g[x+1, y+1] + (1-abs(i/division)) * self.g[x, y+1]
+                    cost = self.heuristic(child, s) + abs(i/division) * self.rhs[x+1, y+1] + (1-abs(i/division)) * self.rhs[x, y+1]
                 else:
-                    cost = 1 + self.g[x, y + 1]
+                    cost = 1 + self.rhs[x, y + 1]
                 if not math.isnan(cost):
                     interpolated_children.put_nowait((cost, child))
             
             child = InterpolatedState(x + i /division, y - 1)
             if self.state_is_valid(child):
                 if i < 0:
-                    cost = self.heuristic(child, s) + abs(i/division) * self.g[x-1,y-1] + (1-abs(i/division)) * self.g[x, y-1]
+                    cost = self.heuristic(child, s) + abs(i/division) * self.rhs[x-1,y-1] + (1-abs(i/division)) * self.rhs[x, y-1]
                 elif i > 0:
-                    cost = self.heuristic(child, s) + abs(i/division) * self.g[x+1, y-1] + (1-abs(i/division)) * self.g[x, y-1]
+                    cost = self.heuristic(child, s) + abs(i/division) * self.rhs[x+1, y-1] + (1-abs(i/division)) * self.rhs[x, y-1]
                 else:
-                    cost = 1 + self.g[x, y - 1]
+                    cost = 1 + self.rhs[x, y - 1]
                 if not math.isnan(cost):
                     interpolated_children.put_nowait((cost, child))
 
@@ -290,34 +360,52 @@ class DStarLight:
         while s != self.s_goal:
             s = self.get_best_interpolated_child(s)
             path.append(s)
+        self.path = path
         return path
-
-
-    def main(self):
-        self.s_last = self.s_start
-        self.compute_shortest_path()
+        
+    def add_obstacles(self, obstacles: list[State]):
+        self.g[self.s_start.value] = np.inf
+        self.rhs[self.s_start.value] = np.inf
+        for obstacle in obstacles:
+            self.costs[obstacle.value] = np.inf
+        path_int = list(map(lambda x: x.value, self.path))
+        obstacle_int = list(map(lambda x: x.value, obstacles))
+        intersection = set(path_int).intersection(obstacle_int)
+        if len(intersection) > 0:
+            self.k_m += self.heuristic(self.s_start, self.s_goal)
+            for point in obstacles:
+                self.rhs[point.value] = np.inf
+                self.g[point.value] = np.inf
+                for neighbour in self.prev(point):
+                    self.rhs[neighbour.value] = np.inf
+                    self.g[neighbour.value] = np.inf
+            for point in intersection:
+                s = State(point[0], point[1])
+                self.update_vertex(s, True)
+                for neighbour in self.prev(s):
+                    self.update_vertex(neighbour, True)
+            self.compute_shortest_path(True)
+        
+        
 
 def main():
     start = time.time()
     costs = np.ones((WIDTH,DEPTH))
-    costs[WIDTH-2, int(DEPTH/2):DEPTH] = np.inf
-    costs[5:WIDTH, DEPTH-12] = np.inf
-    costs[0:WIDTH-5, int(DEPTH/4)] = np.inf
+    costs[25,5:20] = np.inf
+    costs[15, 0:10] = np.inf
+    costs[5:10,5] = np.inf
     d_star = DStarLight(State(0,0),State(WIDTH-1,DEPTH-1), costs)
-    d_star.main()
-    intermediate = time.time()
+    d_star.compute_shortest_path(False)
 
     path = d_star.get_path()
-    end = time.time()
-    print(intermediate - start)
-    print(end-intermediate)
-
+    print("First computation")
+    print(time.time() - start)
     pl.ioff()
     pl.subplots(1, 1, figsize=(10,10))
     pl.grid(True)
     pl.axis("Equal")
     pl.xlim([0,WIDTH])
-    pl.ylim([0,20])
+    pl.ylim([0,DEPTH])
     pl.margins(0)
     pl.plot(0, 0, "ob")
     pl.plot(WIDTH - 1, DEPTH - 1, "og")
@@ -330,6 +418,34 @@ def main():
     pl.plot(x, y, '-r')
     pl.imshow(costs.transpose() > 1, aspect='auto', interpolation='nearest' )
     pl.show()
+
+    start = time.time()
+    new_start = path[15]
+    d_star.s_start = State(new_start.value[0], new_start.value[1])
+    d_star.add_obstacles([State(40,20), State(40,21), State(40,22), State(40,23), State(40,24), State(40,25)])
+    path = d_star.get_path()
+    print("After obstacle")
+    print(time.time() - start)
+    #d_star.plot()
+    pl.ioff()
+    pl.subplots(1, 1, figsize=(10,10))
+    pl.grid(True)
+    pl.axis("Equal")
+    pl.xlim([0,WIDTH])
+    pl.ylim([0,DEPTH])
+    pl.margins(0)
+    pl.plot(0, 0, "ob")
+    pl.plot(WIDTH - 1, DEPTH - 1, "og")
+    x: list[float] = []
+    y: list[float] = []
+    for u in path:
+        x.append(u.to_float()[0])
+        y.append(u.to_float()[1])
+    pl.plot(x, y, '-r')
+    pl.imshow(costs.transpose() > 1, aspect='auto', interpolation='nearest' )
+    pl.show()
+    print("END")
+
     
 
 
