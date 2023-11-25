298af2cb8bc0b46bcd9cadfe4a0d1c7151ba8580
Maxence Leroy <maxence.leroy3@gmail.com>
2023-11-25 09:56:07 +0100

    move 10 cm for PID adjustments

--- potato/main.py
+++ potato/main.py
@@ -1,11 +1,9 @@
 import asyncio
-import math
 import time
 
-from src.action import ActionsSequence, Move, Wait
+from src.action import ActionsSequence, Move
 from src.constants import MATCH_TIME, Side, ROBOT_WIDTH, ROBOT_DEPTH
-from src.location.location import MoveForward, Coordinates, RelativeMove
-from src.location.best_available import BestAvailable, ImportantLocation
+from src.location.location import MoveForward, Coordinates
 from src.logging import logging_info, start, logging_error
 from src.playing_area import playing_area
 from src.replay.save_replay import start_replay, open_replay_file
@@ -17,49 +15,7 @@ def main():
         timer_limit=MATCH_TIME,
         actions=[
             Move(
-                destination=BestAvailable(ImportantLocation.PLANT),
-                pathfinding=True
-            ),
-            Move(
-                destination = MoveForward(10)
-            ),
-            Move(
-                destination=MoveForward(-10),
-                backwards=True
-            ),
-            Wait(1.0),
-            Move(
-                destination=BestAvailable(ImportantLocation.POT),
-                pathfinding=True
-            ),
-            Move(
-                destination=MoveForward(-200),
-                backwards=True
-            ),
-            Move(
-                destination=RelativeMove(0, 0, math.pi),
-            ),
-            Move(
-                destination=MoveForward(-210),
-                backwards=True
-            ),
-            Wait(1.0),
-            Move(
-                destination=BestAvailable(ImportantLocation.PLANTER),
-                pathfinding=True
-            ),
-            Move(
-                destination=RelativeMove(0, 0, math.pi),
-                forced_angle=True
-            ),
-            Move(
-                destination=MoveForward(-10),
-                backwards=True
-            ),
-            Wait(1.0),
-            Move(
-                destination=Coordinates(0, 0, 0),
-                pathfinding=True
+                MoveForward(100)
             )
         ],
         allows_fail=False
